092a4443303de0fe35c8d10d5768662f7108c0e4
We5t <vitaliy14042007@gmail.com>
2025-05-02 16:12:37 +0200

    Updates

---
+++ static/js/289.fa5d8e06.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[289],{289:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.fa5d8e06.chunk.js.map
